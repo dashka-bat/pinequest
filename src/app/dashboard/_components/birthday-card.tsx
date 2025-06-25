fbfcc0754e37e628d7da79a3dac983c7373a78de
dashka-bat <dushkab02@gmail.com>
2025-06-25 17:41:59 +0800

    done

--- src/app/dashboard/_components/birthday-card.tsx
+++ src/app/dashboard/_components/birthday-card.tsx
@@ -1,8 +1,9 @@
+'use client';
 import React from 'react';
 
 const BirthdayCard = () => {
   return (
-    <div className="flex justify-between items-center rounded-3xl p-10 w-full bg-gradient-to-r from-[#7EA9BD] to-[#DDE5E8] text-white">
+    <div className=" relative flex justify-between items-center rounded-3xl p-10 w-full bg-gradient-to-r from-[#FF5E5E] to-[#FFFFFF] text-white">
       <div>
         <h2 className="text-3xl font-semibold leading-tight">
           Уянгадарь-н
@@ -11,7 +12,7 @@ const BirthdayCard = () => {
         </h2>
         <p className="mt-2 text-sm">Мэндчилгээг дэвшүүлээрэй</p>
       </div>
-      <div className="text-black text-2xl font-semibold">Өнөөдөр</div>
+      <div className="text-black text-2xl font-semibold">Өнөөдөр</div>{' '}
     </div>
   );
 };
---
+++ src/app/dashboard/_components/birthday-card2.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BirthdayCard2 = () => {
+  return (
+    <div className="flex justify-between items-center rounded-3xl p-10 w-full bg-gradient-to-r from-[#1980FA] to-[#E1EFFE] text-white">
+      <div>
+        <h2 className="text-3xl font-semibold leading-tight">
+          Мөнх-Оргил-н
+          <br />
+          Төрсөн өдөр
+        </h2>
+        <p className="mt-2 text-sm">Мэндчилгээг дэвшүүлээрэй</p>
+      </div>
+      <div className="text-black text-2xl font-semibold">53 Хоног</div>
+    </div>
+  );
+};
+
+export default BirthdayCard2;
