7a2610b17587d46806c72bff0c2cbf01382aa059
“dashka-bat” <dushkab02@gmail.com>
2025-07-01 11:06:51 +0800

    done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,7 @@ import Dashboard from './_featured/dashboard';
 import GratitudeTab from './_featured/gratitude';
 import NewCard from './_featured/new-card';
 import EventsTab from './_featured/events';
-<<<<<<< HEAD
 import UserTab from './_featured/user';
-=======
-import Users from './_featured/users';
-import Profile from './_featured/profile';
->>>>>>> main
 
 type MainProps = {
   searchParams: Promise<{
