3ec1ca9cb2d56a920ead45f7d5b8e834de9f64ab
“dashka-bat” <dushkab02@gmail.com>
2025-07-01 12:33:30 +0800

    done

--- src/app/dashboard/_components/sidebar.tsx
+++ src/app/dashboard/_components/sidebar.tsx
@@ -2,16 +2,25 @@
 
 import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
-import { Home, StickyNote, LogOut, PartyPopper,Pencil, UsersRound, UserRound, ChevronDown, HelpCircle, } from 'lucide-react';
+import {
+  Home,
+  StickyNote,
+  LogOut,
+  PartyPopper,
+  Pencil,
+  UsersRound,
+  UserRound,
+  ChevronDown,
+  // HelpCircle,
+} from 'lucide-react';
 
 import clsx from 'clsx';
-import { logout } from './logout';
+// import { logout } from './logout';
 
 const menu = [
-   {label: "Миний булан",href:"/dashboard?tab=profle",tab:"profile",icon:UserRound},
+  { label: 'Миний булан', href: '/dashboard?tab=profle', tab: 'profile', icon: UserRound },
   { label: 'Нүүр', href: '/dashboard', tab: null, icon: Home },
 
-  
   { label: 'Талархал бичих', href: '/dashboard?tab=new-card', tab: 'new-card', icon: Pencil },
   { label: 'Тэмдэглэлт өдрүүд', href: '/dashboard?tab=events', tab: 'events', icon: PartyPopper },
   {
@@ -20,45 +29,43 @@ const menu = [
     tab: 'gratitude',
     icon: StickyNote,
   },
-  { label : "Бүх ажилчин", href : "/dashboard?tab=user", tab : "user",icon : UsersRound },
-
- 
+  { label: 'Бүх ажилчин', href: '/dashboard?tab=user', tab: 'user', icon: UsersRound },
 ];
 
 const DashboardSidebar = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentTab = searchParams.get('tab');
-  const isUserTab = currentTab === 'user';
+  // const isUserTab = currentTab === 'user';
 
   return (
     <div className="w-[100%] max-w-xs bg-white p-12 h-screen">
       <div className="flex flex-col justify-between h-full">
         <nav className="flex flex-col gap-12 text-sm text-gray-700">
-         
           {menu.map(({ label, href, tab, icon: Icon }) => {
             const isActive =
               (pathname === '/dashboard' && !tab && !currentTab) || currentTab === tab;
-              if(tab==="profile"){
-                return (  <Link href={"/dashboard?tab=profile"} key={12}>
-              <div className='rounded-[16px] border-[1px] border-solid border-[#E6E7EC] w-[226px] h-[72px] flex justify-between items-center'>
-                <div className='flex justify-center items-center'>
-                  <div className='flex gap-[16px] ml-[16px]'>
-                  <img className='w-[40px] h-[40px] rounded-full' src={"/half.png"}></img>
-                  <div>
-                    <div>Name</div>
-                    <div>Миний булан</div>
+            if (tab === 'profile') {
+              return (
+                <Link href={'/dashboard?tab=profile'} key={12}>
+                  <div className="rounded-[16px] border-[1px] border-solid border-[#E6E7EC] w-[226px] h-[72px] flex justify-between items-center">
+                    <div className="flex justify-center items-center">
+                      <div className="flex gap-[16px] ml-[16px]">
+                        <img className="w-[40px] h-[40px] rounded-full" src={'/half.png'}></img>
+                        <div>
+                          <div>Name</div>
+                          <div>Миний булан</div>
+                        </div>
+                      </div>
+                      <div className="ml-[32px]">
+                        {' '}
+                        <ChevronDown />
+                      </div>
+                    </div>
                   </div>
-                  
-                </div>
-               <div className='ml-[32px]'> <ChevronDown /></div>
-
-                </div>
-
-              </div>
-
-          </Link>)
-              }
+                </Link>
+              );
+            }
             return (
               <Link
                 key={href}
@@ -72,13 +79,9 @@ const DashboardSidebar = () => {
                 {label}
               </Link>
             );
-            
           })}
-
-         
         </nav>
         <div className="flex flex-col gap-4 pt-8 text-sm">
-        
           <Link
             href="/logout"
             className="flex items-center gap-3 text-red-500 hover:text-red-600 transition-all"
